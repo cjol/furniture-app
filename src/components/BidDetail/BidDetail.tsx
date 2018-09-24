bc5af59a90737ef361533690cf9e2544262a93d2
user006 <user006@tyrohq.com>
2018-09-24 01:42:55 -0700

    1.1 BidDetailCompleted

---
+++ src/components/BidDetail/BidDetail.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+import { Placeholder } from "../../style/Placeholder";
+
+export class BidDetail extends React.PureComponent<{}> {
+  static defaultProps: BidDetail["props"] = {};
+
+  render() {
+    return (
+      <Placeholder description="BidPageComponent" orientation="row">
+        <Placeholder description="PriceAndTime" orientation="row">
+          <Placeholder description="BidPrice" />
+          <Placeholder description="TimeTaken" />
+        </Placeholder>
+        <Placeholder description="BidText" />
+        <Placeholder description="Select Supplier" />
+      </Placeholder>
+    );
+  }
+}

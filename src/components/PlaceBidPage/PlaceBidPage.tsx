a6aa6ac7a9759d59bc66c3b291cccc7dff82bceb
User 002 <user002@tyrohq.com>
2018-09-24 13:57:49 +0300

    IncludeFile

--- src/components/PlaceBidPage/PlaceBidPage.tsx
+++ src/components/PlaceBidPage/PlaceBidPage.tsx
@@ -1,14 +1,43 @@
 import * as React from "react";
 import { Placeholder } from "Components/Placeholder";
+import { AppHeader } from "Components/AppHeader/AppHeader";
 
 export class PlaceBidPage extends React.PureComponent<{}> {
-  static defaultProps: PlaceBidPage ["props"] = {};
+  static defaultProps: PlaceBidPage["props"] = {};
 
   render() {
-    return <Placeholder description="MenuBar" orientation="row">
-            <Placeholder description="MenuButton"/>
-            <Placeholder description="Title"/>
-    </Placeholder>
-    ;
+    return (
+      <Placeholder description="Page" orientation="column">
+        <AppHeader />
+        <Placeholder description="MenuBar" orientation="row">
+          <Placeholder description="MenuButton" />
+          <Placeholder description="Title" />
+        </Placeholder>
+        <Placeholder description="Bids" orientation="row">
+          <Placeholder description="BidsNumber" />
+          <Placeholder description="AverageBid" />
+        </Placeholder>
+        <Placeholder description="AuctionClosing" />
+
+        <Placeholder description="ImageBar" orientation="row">
+          <Placeholder description="LeftButton" />
+          <Placeholder description="Image1" />
+          <Placeholder description="Image2" />
+          <Placeholder description="Image3" />
+        </Placeholder>
+
+        <Placeholder description="ProjectDescription" />
+
+        <Placeholder description="PlaceBid" orientation="column">
+          <Placeholder description="Price" />
+          <Placeholder description="TimeToComplete" />
+          <Placeholder description="AddBidDescription">
+            <Placeholder description="Description1" />
+            <Placeholder description="Description2" />
+            <Placeholder description="PlaceBidButton" />
+          </Placeholder>
+        </Placeholder>
+      </Placeholder>
+    );
   }
 }

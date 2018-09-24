d836f74d068c731ce6d4634c4d5526fa55961054
Christopher Little <christopher@tyrohq.com>
2018-09-24 12:22:02 +0300

    Minor change to header

--- src/components/ProjectHeader/ProjectHeader.tsx
+++ src/components/ProjectHeader/ProjectHeader.tsx
@@ -1,16 +1,13 @@
 import * as React from "react";
 import { Placeholder } from "Components/Placeholder";
 
-
 export class ProjectHeader extends React.PureComponent<{}> {
   static defaultProps: ProjectHeader["props"] = {};
 
   render() {
     return (
       <Placeholder description="Project header" orientation="column">
-        <Placeholder description="ProjectTitle" orientation="column">
-          <Placeholder description="SelectProject" />
-        </Placeholder>
+        <Placeholder description="ProjectTitle" orientation="column" />
         <Placeholder description="ProjectOwner" orientation="column">
           <Placeholder description="OwnerPicture" />
           <Placeholder description="OwnerName" />

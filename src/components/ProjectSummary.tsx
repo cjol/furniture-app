0759d354229c3f281dc4af7703e131571fe625d9
Christopher Little <christopher@tyrohq.com>
2018-09-24 12:38:47 +0300

    Minor changes

--- src/components/ProjectSummary.tsx
+++ src/components/ProjectSummary.tsx
@@ -6,31 +6,51 @@ export class ProjectSummary extends React.PureComponent<{}> {
   static defaultProps: ProjectSummary["props"] = {};
 
   render() {
-    return <Placeholder description="Component replaces this" />;
-    <Placeholder description="Page" orientation="column">
-      <Placeholder description="Header" orientation="row">
-        <Placeholder description="Menu Bar" />
-        <Placeholder description="Project Listings" />
-      </Placeholder>
-      <Placeholder description="SearchContainer">
-        <Placeholder description="Search Projects" />
-        <Placeholder description="Search icon" />
-      </Placeholder>
-      <Placeholder description="Listings Container" orientation="row">
-        <Placeholder description="Listings" orientation="column">
-          <Placeholder description="Idividual Project" orientation="row">
-            <Placeholder description="Project Image" />
-            <Placeholder description="Project Title" orientation="row">
-              <Placeholder description="Project Tags" />
-              <Placeholder description="Project Price" />
-            </Placeholder>
-          </Placeholder>
+    return (
+      <Placeholder description="Page" orientation="column">
+        <Placeholder description="Header" orientation="row">
+          <Placeholder description="Menu Bar" />
+          <Placeholder description="Project Listings" />
+        </Placeholder>
+        <Placeholder description="SearchContainer">
+          <Placeholder description="Search Projects" />
+          <Placeholder description="Search icon" />
+        </Placeholder>
+        <Placeholder description="Listings Container" orientation="column">
           <Placeholder description="Listings Info">
+            <Placeholder description="Listings Title" />
             <Placeholder description="Up and Down Arrows" />
-            <Placeholder description="Date" />
+            <Placeholder description="Avg. Bid" />
+          </Placeholder>
+
+          <Placeholder description="Individual Project" orientation="row">
+            <Placeholder description="Project Image" />
+            <Placeholder description="Project Info" orientation="column">
+              <Placeholder description="Project Title" orientation="row" />
+              <Placeholder description="Project Tags" />
+            </Placeholder>
+            <Placeholder description="Project Price" />
+          </Placeholder>
+
+          <Placeholder description="Individual Project" orientation="row">
+            <Placeholder description="Project Image" />
+            <Placeholder description="Project Info" orientation="column">
+              <Placeholder description="Project Title" orientation="row" />
+              <Placeholder description="Project Tags" />
+            </Placeholder>
+            <Placeholder description="Project Price" />
+          </Placeholder>
+
+          <Placeholder description="Individual Project" orientation="row">
+            <Placeholder description="Project Image" />
+            <Placeholder description="Project Info" orientation="column">
+              <Placeholder description="Project Title" orientation="row" />
+              <Placeholder description="Project Tags" />
+            </Placeholder>
+            <Placeholder description="Project Price" />
           </Placeholder>
         </Placeholder>
       </Placeholder>
-    </Placeholder>;
+    );
   }
 }

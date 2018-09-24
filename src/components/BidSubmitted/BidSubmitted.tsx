d88ef4d6acdfa5227f921d2e68948f7dcf432ec2
user005 <user005@tyrohq.com>
2018-09-24 11:13:11 -0700

    completed

--- src/components/BidSubmitted/BidSubmitted.tsx
+++ src/components/BidSubmitted/BidSubmitted.tsx
@@ -1,13 +1,14 @@
 import * as React from "react";
 import { Placeholder } from "Components/Placeholder";
 
-export class __ComponentName__ extends React.PureComponent<{}> {
-  static defaultProps: __ComponentName__["props"] = {};
+export class BidSubmitted extends React.PureComponent<{}> {
+  static defaultProps:BidSubmitted ["props"] = {};
 
   render() {
     return (
       <Placeholder description="Home Page">
         <Placeholder description="Bid Title" />
+        <Placeholder description="Close Button"/>
         <Placeholder description="Response Message" />
       </Placeholder>
     );

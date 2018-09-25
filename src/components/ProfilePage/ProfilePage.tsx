97bde9cd21242a61fe06186866200af2fbd519cd
User 001 <user001@tyrohq.com>
2018-09-25 14:47:31 +0300

    Completed 1.3 ProfilePageFileEdit

--- src/components/ProfilePage/ProfilePage.tsx
+++ src/components/ProfilePage/ProfilePage.tsx
@@ -1,10 +1,10 @@
 import * as React from "react";
 import { Placeholder } from "Components/Placeholder";
 import { AppHeader } from "Components/AppHeader/AppHeader";
-import { ProfileEditSave } from "Components/ProfilePage/ProfileEditSave/ProfileEditSave";
-import { ActiveProjectBids } from "Components/ProfilePage/ActiveProjectBids/ActiveProjectBids";
-import { SupplierSelected } from "Components/ProfilePage/SupplierSelected/SupplierSelected";
-import { ProfileDetails } from "Components/ProfilePage/ProfileDetails/ProfileDetails";
+import { ProfileEditSave } from "./ProfileEditSave/ProfileEditSave";
+import { ActiveProjectBids } from "./ActiveProjectBids/ActiveProjectBids";
+import { SupplierSelected } from "./SupplierSelected/SupplierSelected";
+import { ProfileDetails } from "./ProfileDetails/ProfileDetails";
 
 export class ProfilePage extends React.PureComponent<{}> {
   static defaultProps: ProfilePage["props"] = {};

7e2b20339faa4120443818aa5da43b621b1abb65
Hama <user006@tyrohq.com>
2018-09-25 14:20:21 +0300

    2.1SubHeadingComplete

--- src/components/Subheading/Subheading.tsx
+++ src/components/Subheading/Subheading.tsx
@@ -1,10 +1,13 @@
 import * as React from "react";
 import { Placeholder } from "Components/Placeholder";
-
+import styled from "styled-components";
+const SubHeadingStyled = styled.div`
+  display: flex;
+`;
 export class Subheading extends React.PureComponent<{}> {
   static defaultProps: Subheading["props"] = {};
 
   render() {
-    return <Placeholder description="SUBHEADING" />;
+    return <SubHeadingStyled />;
   }
 }

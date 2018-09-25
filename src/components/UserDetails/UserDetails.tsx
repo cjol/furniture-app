35d85ec2e850be61a17fa2b9b6e9a0e79a82e7bc
Hama <user006@tyrohq.com>
2018-09-25 09:46:37 +0300

    2.1.2.2UserDetailsCompleted

--- src/components/UserDetails/UserDetails.tsx
+++ src/components/UserDetails/UserDetails.tsx
@@ -1,7 +1,21 @@
 import { Placeholder } from "Components/Placeholder";
+import { Star, StarBorder } from "@material-ui/icons";
+import styled from "styled-components";
 import { ProfileRatings } from "Components/ProfilePage/ProfileDetails/ProfileRatings/ProfileRatings";
 import * as React from "react";
 
+const UserDetailsStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const UserPicturestyled = styled.div`
+  display: flex;
+  border-radius: 30px;
+`;
+const RatingStarsStyled = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
 export class UserDetails extends React.PureComponent<{}> {
   static defaultProps: UserDetails["props"] = {};
 

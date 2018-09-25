7a5f8eaa7c3690cc79fabcd61a0675b828856ebf
Christopher Little <christopher@tyrohq.com>
2018-09-25 17:03:18 +0300

    Re-add placeholder title

--- src/components/ProjectHeader/ProjectTitle/ProjectTitle.tsx
+++ src/components/ProjectHeader/ProjectTitle/ProjectTitle.tsx
@@ -8,6 +8,6 @@ export class ProjectTitle extends React.PureComponent<{}> {
   static defaultProps: ProjectTitle["props"] = {};
 
   render() {
-    return <ProjectTitleStyle />;
+    return <ProjectTitleStyle>Title Goes Here</ProjectTitleStyle>;
   }
 }

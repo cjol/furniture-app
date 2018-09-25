abc48e1f0d81e7afa20581ae22cf736439ef8425
Hama <user006@tyrohq.com>
2018-09-25 11:51:19 +0300

    1.1+1.2FilterResultsProjectTagsComplete

--- src/components/FilterResultsPopUp/FilterResultsProjectTags/FilterResultsProjectTags.tsx
+++ src/components/FilterResultsPopUp/FilterResultsProjectTags/FilterResultsProjectTags.tsx
@@ -1,20 +1,25 @@
 import { Placeholder } from "Components/Placeholder";
 import { ProjectTag } from "./ProjectTag/ProjectTag";
 import * as React from "react";
+import styled from "styled-components";
 
+const FilterResultsProjectTagsStyled = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
 export class FilterResultsProjectTags extends React.PureComponent<{}> {
   static defaultProps: FilterResultsProjectTags["props"] = {};
 
   render() {
     return (
-      <Placeholder description="FilterResultsProjectTags" orientation="row">
+      <FilterResultsProjectTagsStyled>
         <ProjectTag />
         <ProjectTag />
         <ProjectTag />
         <ProjectTag />
         <ProjectTag />
         <ProjectTag />
-      </Placeholder>
+      </FilterResultsProjectTagsStyled>
     );
   }
 }

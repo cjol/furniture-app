9e6b1ed13c4648275512e483fd1e5149a42fc2ab
Christopher Little <christopher@tyrohq.com>
2018-09-25 11:39:02 +0300

    0.0-TagBoxes Version 1

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -17,14 +17,12 @@ import { PhotoUploadPopUp } from "Components/PhotoUploadPopUp/PhotoUploadPopUp";
 import { PlaceBidPage } from "Components/PlaceBidPage/PlaceBidPage";
 import { ProfilePage } from "Components/ProfilePage/ProfilePage";
 import { ProjectHeader } from "Components/ProjectHeader/ProjectHeader";
-import { ProjectListingsPage } from "Components/ProjectListingsPage/ProjectListingsPage";
-import { ProjectSummary } from "Components/ProjectSummary/ProjectSummary";
+import { ProjectListings } from "Components/ProjectListings/ProjectListings";
 import { RectangularButton } from "Components/RectangularButton/RectangularButton";
 import { RoundButton } from "Components/RoundButton/RoundButton";
 import { SubmitQuestionPopUp } from "Components/SubmitQuestionPopUp/SubmitQuestionPopUp";
 
 import "@assets/scss/App.scss";
-import { ProfilePage } from "Components/ProfilePage/Profilepage";
 
 const client = new ApolloClient({
   uri:
@@ -72,11 +70,7 @@ export default class App extends React.Component {
                 <Route path="/PlaceBidPage" component={PlaceBidPage} />
                 <Route path="/ProfilePage" component={ProfilePage} />
                 <Route path="/ProjectHeader" component={ProjectHeader} />
-                <Route
-                  path="/ProjectListingsPage"
-                  component={ProjectListingsPage}
-                />
-                <Route path="/ProjectSummary" component={ProjectSummary} />
+                <Route path="/ProjectListings" component={ProjectListings} />
                 <Route
                   path="/RectangularButton"
                   component={RectangularButton}
@@ -125,12 +119,7 @@ export default class App extends React.Component {
                         <Link to="/ProjectHeader">ProjectHeader</Link>
                       </li>
                       <li>
-                        <Link to="/ProjectListingsPage">
-                          ProjectListingsPage
-                        </Link>
-                      </li>
-                      <li>
-                        <Link to="/ProjectSummary">ProjectSummary</Link>
+                        <Link to="/ProjectListings">ProjectListings</Link>
                       </li>
                       <li>
                         <Link to="/RectangularButton">RectangularButton</Link>

c010f8cf84e37dfc97f525a7b6b7bc3799206565
Christopher Little <christopher@tyrohq.com>
2018-09-25 21:03:30 +0300

    forgot t save

--- src/components/PhotoUploadPopUp/PhotoUploadPopUp.tsx
+++ src/components/PhotoUploadPopUp/PhotoUploadPopUp.tsx
@@ -1,21 +1,20 @@
-import { Add, Close } from "@material-ui/icons";
-import { Placeholder } from "Components/Placeholder";
+import { Add } from "@material-ui/icons";
+import { Popup } from "Components/Popup/Popup";
 import { RectangularButton } from "Components/RectangularButton/RectangularButton";
-import { PhotoUploadHeader } from "./PhotoUploadHeader/PhotoUploadHeader";
-import { PhotoUrl } from "./PhotoUrl/PhotoUrl";
 import * as React from "react";
+
+import { PhotoUrl } from "./PhotoUrl/PhotoUrl";
 
 export class PhotoUploadPopUp extends React.PureComponent<{}> {
   static defaultProps: PhotoUploadPopUp["props"] = {};
 
   render() {
     return (
-      <Placeholder description="PhotoUploadBox" orientation="column">
-        <PhotoUploadHeader />
+      <Popup>
         <PhotoUrl />
         <Add />
         <RectangularButton />
-      </Placeholder>
+      </Popup>
     );
   }
 }

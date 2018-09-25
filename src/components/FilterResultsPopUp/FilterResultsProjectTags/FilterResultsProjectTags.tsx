45870acec0493114340ec404d0c9995d93e01369
User 001 <user001@tyrohq.com>
2018-09-25 15:41:24 +0300

    Completed 1.3 FilterResultsPopUp

---
+++ src/components/FilterResultsPopUp/FilterResultsProjectTags/FilterResultsProjectTags.tsx
@@ -0,0 +1,43 @@
+import * as React from "react";
+import { Placeholder } from "Components/Placeholder";
+import { CheckBox } from "@material-ui/icons";
+import { CheckBoxOutlineBlank } from "@material-ui/icons";
+
+export class FilterResultsProjectTags extends React.PureComponent<{}> {
+  static defaultProps: FilterResultsProjectTags["props"] = {};
+
+  render() {
+    return (
+      <Placeholder description="FilterResultsProjectTags" orientation="column">
+        <Placeholder description="FilterResultsProjectTagsRow1">
+          <Placeholder description="FilterResultsProjectTagBed">
+            <Placeholder description="FilterResultTagBed" />
+            <CheckBox />
+          </Placeholder>
+          <Placeholder description="FilterResultsProjectTagchair">
+            <Placeholder description="FilterResultTagChair" />
+            <CheckBox />
+          </Placeholder>
+          <Placeholder description="FilterResultsProjectTagTable">
+            <Placeholder description="FilterResultTagTable" />
+            <CheckBoxOutlineBlank />
+          </Placeholder>
+        </Placeholder>
+        <Placeholder description="FilterResultsProjectTagsRow2">
+          <Placeholder description="FilterResultsProjectTagBedSideTable">
+            <Placeholder description="FilterResultTagBedSideTable" />
+            <CheckBoxOutlineBlank />
+          </Placeholder>
+          <Placeholder description="FilterResultsProjectTagDesk">
+            <Placeholder description="FilterResultTagDesk" />
+            <CheckBoxOutlineBlank />
+          </Placeholder>
+          <Placeholder description="FilterResultsProjectTagSofa">
+            <Placeholder description="FilterResultTagSofa" />
+            <CheckBoxOutlineBlank />
+          </Placeholder>
+        </Placeholder>
+      </Placeholder>
+    );
+  }
+}

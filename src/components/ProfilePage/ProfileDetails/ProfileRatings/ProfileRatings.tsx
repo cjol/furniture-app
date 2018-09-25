09918a3d3c0f6ab2ea76960f03a7074dc6606df4
user004 <user004@tyrohq.com>
2018-09-25 16:45:20 -0700

    2.1-ProfileRatings

--- src/components/ProfilePage/ProfileDetails/ProfileRatings/ProfileRatings.tsx
+++ src/components/ProfilePage/ProfileDetails/ProfileRatings/ProfileRatings.tsx
@@ -2,19 +2,23 @@ import * as React from "react";
 import { Placeholder } from "Components/Placeholder";
 import { Star } from "@material-ui/icons";
 import { StarBorder } from "@material-ui/icons";
+import styled from "styled-components";
+
+const RatingsStyle=styled.div``;
+
 
 export class ProfileRatings extends React.PureComponent<{}> {
   static defaultProps: ProfileRatings["props"] = {};
 
   render() {
     return (
-      <Placeholder description="Ratings">
+      <RatingsStyle>
         <Star />
         <Star />
         <Star />
         <Star />
         <StarBorder />
-      </Placeholder>
+      </RatingsStyle>
     );
   }
 }

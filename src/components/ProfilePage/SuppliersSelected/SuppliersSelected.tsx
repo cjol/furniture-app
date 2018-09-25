c94272675ceb99938a2ec5343e871a1fb7b4d60d
Hama <user006@tyrohq.com>
2018-09-25 12:36:20 +0300

    2.1+2.2SupplierSelectedCompleted

--- src/components/ProfilePage/SuppliersSelected/SuppliersSelected.tsx
+++ src/components/ProfilePage/SuppliersSelected/SuppliersSelected.tsx
@@ -2,18 +2,23 @@ import * as React from "react";
 import { Placeholder } from "Components/Placeholder";
 import { ProjectListing } from "Components/ProjectListing/ProjectListing";
 import { Subheading } from "Components/Subheading/Subheading";
+import styled from "styled-components";
 
+const SuppliersSelectedStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 export class SuppliersSelected extends React.PureComponent<{}> {
   static defaultProps: SuppliersSelected["props"] = {};
 
   render() {
     return (
-      <Placeholder description="Selected Suppliers" orientation="column">
+      <SuppliersSelectedStyled>
         <Subheading />
         <ProjectListing />
         <ProjectListing />
         <ProjectListing />
-      </Placeholder>
+      </SuppliersSelectedStyled>
     );
   }
 }

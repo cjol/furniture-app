ead2f117694dad5a07a16237c6d9d512ddca5819
Hama <user006@tyrohq.com>
2018-09-25 11:57:36 +0300

    2.1+2.2 InfoRowPictureStyleComplete

--- src/components/BidResolutionPage/UserContactInfo/InfoRow/InfoRowPicture/InfoRowPicture.tsx
+++ src/components/BidResolutionPage/UserContactInfo/InfoRow/InfoRowPicture/InfoRowPicture.tsx
@@ -1,10 +1,13 @@
 import * as React from "react";
 import { Placeholder } from "Components/Placeholder";
-
+import styled from "styled-components";
+const InfoRowPictureStyled = styled.div`
+  display: flex;
+`;
 export class InfoRowPicture extends React.PureComponent<{}> {
   static defaultProps: InfoRowPicture["props"] = {};
 
   render() {
-    return <Placeholder description="Picture" />;
+    return <InfoRowPictureStyled />;
   }
 }

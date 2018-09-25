5a16468e39c228d4dec6a4bbd195ea35162c43bb
Hama <user006@tyrohq.com>
2018-09-25 15:34:40 +0300

    2.4BidResolutionPage/InfoNameAndNumberComplete

--- src/components/BidResolutionPage/UserContactInfo/InfoRow/InfoNameAndNumber/InfoNameAndNumber.tsx
+++ src/components/BidResolutionPage/UserContactInfo/InfoRow/InfoNameAndNumber/InfoNameAndNumber.tsx
@@ -1,13 +1,21 @@
 import * as React from "react";
 import styled from "styled-components";
+import { mainFont } from "@style";
 
 const NameAndPhoneNumberStyle = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
 `;
-const NameStyle = styled.div``;
-const PhoneNumberStyle = styled.div``;
+const NameStyle = styled.div`
+  display: flex;
+  font: ${mainFont};
+  font-size: 16px;
+`;
+const PhoneNumberStyle = styled.div`
+  display: flex;
+  font-size: 14px;
+`;
 
 export class InfoNameAndNumber extends React.PureComponent<{
   name: string;

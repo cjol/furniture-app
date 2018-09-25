651a95cc8f10e64bf2909ca2bd3aa5a5e484da72
User 002 <user002@tyrohq.com>
2018-09-25 13:16:44 +0300

    1.2-ProjectListings

--- src/components/ProjectListings/ProjectListings.tsx
+++ src/components/ProjectListings/ProjectListings.tsx
@@ -1,5 +1,10 @@
 import * as React from "react";
 import { Placeholder } from "Components/Placeholder";
+import { Menu } from "@material-ui/icons";
+import { Search } from "@material-ui/icons";
+import { ArrowDropDown } from "@material-ui/icons";
+import { ArrowDropUp } from "@material-ui/icons";
+
 
 export class ProjectListings extends React.PureComponent<{}> {
   static defaultProps: ProjectListings["props"] = {};
@@ -8,17 +13,18 @@ export class ProjectListings extends React.PureComponent<{}> {
     return (
       <Placeholder description="Page" orientation="column">
         <Placeholder description="Header" orientation="row">
-          <Placeholder description="Menu Bar" />
+          <Menu/>
           <Placeholder description="Project Listings" />
         </Placeholder>
-        <Placeholder description="SearchContainer">
+        <Placeholder description="SearchContainer" orientation="row">
           <Placeholder description="Search Projects" />
-          <Placeholder description="Search icon" />
+          <Search/>
         </Placeholder>
         <Placeholder description="Listings Container" orientation="column">
-          <Placeholder description="Listings Info">
+          <Placeholder description="Listings Info" orientation="row">
             <Placeholder description="Listings Title" />
-            <Placeholder description="Up and Down Arrows" />
+            <ArrowDropUp/>
+            <ArrowDropDown/>
             <Placeholder description="Avg. Bid" />
           </Placeholder>
 

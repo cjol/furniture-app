ca30aec116a1018b531abce6ddb29d0b1bd424af
user003 <user003@tyrohq.com>
2018-09-26 09:48:03 -0700

    2.1+2.2-ImageBar

--- src/components/ImageBar/ImageBar.tsx
+++ src/components/ImageBar/ImageBar.tsx
@@ -2,19 +2,24 @@ import * as React from "react";
 import { Placeholder } from "Components/Placeholder";
 import { ChevronLeft } from "@material-ui/icons";
 import { ChevronRight } from "@material-ui/icons";
+import styled from "styled-components";
 
+const ImageBarStyle = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
 export class ImageBar extends React.PureComponent<{}> {
   static defaultProps: ImageBar["props"] = {};
 
   render() {
     return (
-      <Placeholder description="ImageBar" orientation="row">
+      <ImageBarStyle>
         <ChevronLeft />
         <Placeholder description="Image1" />
         <Placeholder description="Image2" />
         <Placeholder description="Image3" />
         <ChevronRight />
-      </Placeholder>
+      </ImageBarStyle>
     );
   }
 }

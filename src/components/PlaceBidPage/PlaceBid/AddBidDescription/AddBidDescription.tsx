fcbaa314c254f90dc83d10fbf793783e23246b77
User 001 <user001@tyrohq.com>
2018-09-26 12:33:51 +0300

    Completed 2.1+2.2 PlaceBidPage-PlaceBid-AddBidResolution

--- src/components/PlaceBidPage/PlaceBid/AddBidDescription/AddBidDescription.tsx
+++ src/components/PlaceBidPage/PlaceBid/AddBidDescription/AddBidDescription.tsx
@@ -2,17 +2,33 @@ import { Placeholder } from "Components/Placeholder";
 import { PlainText } from "Components/PlainText/PlainText";
 import { Subheading } from "Components/Subheading/Subheading";
 import * as React from "react";
+import styled from "styled-components";
+
+const AddBidDescriptionStyle = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const DescriptionFeildStyle = styled.div`
+  display: flex;
+  align-items: flex-start;
+  flex-grow: 1;
+`;
 
 export class AddBidDescription extends React.PureComponent<{}> {
   static defaultProps: AddBidDescription["props"] = {};
 
   render() {
     return (
-      <Placeholder description="AddBidDescription" orientation="column">
+      <AddBidDescriptionStyle>
         <Subheading />
         <PlainText />
-        <Placeholder description="DescriptionField" />
-      </Placeholder>
+        <DescriptionFeildStyle>
+          Make sure your bid is submitted by describing what you will do and why
+          you are the best choice.
+        </DescriptionFeildStyle>
+      </AddBidDescriptionStyle>
     );
   }
 }

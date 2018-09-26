74314c745ba9ee5739be209842b3043be5f165fd
Christopher Little <christopher@tyrohq.com>
2018-09-26 10:19:49 +0300

    Merge branch '2.2ProjectHeader-ProjectTitle'

--- src/components/DetailedProjectPage/AboutBid/AboutBid.tsx
+++ src/components/DetailedProjectPage/AboutBid/AboutBid.tsx
@@ -1,13 +1,13 @@
 import * as React from "react";
-import { Placeholder } from "Components/Placeholder";
-import styled from "Styled-components";
-const AboutBidStyle=styled.div`
-display:flex;
-flex-direction:row;
+import styled from "styled-components";
+
+const AboutBidStyle = styled.div`
+  display: flex;
+  flex-direction: row;
 `;
-const NoOfBidsStyle=styled.div``;
-const AverageBidStyle=styled.div``;
-const AuctionClosesStyle=styled.div``;
+const NoOfBidsStyle = styled.div``;
+const AverageBidStyle = styled.div``;
+const AuctionClosesStyle = styled.div``;
 
 export class AboutBid extends React.PureComponent<{}> {
   static defaultProps: AboutBid["props"] = {};

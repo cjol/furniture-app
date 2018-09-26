7d8a060d2e81081b4c6bc9dc1c7f5b58d0b14e3e
user003 <user003@tyrohq.com>
2018-09-25 18:51:38 -0700

    1.3-CreateProjectPage-TaggingProjects 2

--- src/components/CreateProjectPage/TaggingProject/TaggingProject.tsx
+++ src/components/CreateProjectPage/TaggingProject/TaggingProject.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { Placeholder } from "Components/Placeholder";
-import {TagYourProject} from "./TagYourProject/TagYourProject";
+import { TagOptions } from "./TagOptions/TagOptions";
 
 export class TaggingProject extends React.PureComponent<{}> {
   static defaultProps: TaggingProject["props"] = {};
@@ -8,13 +8,9 @@ export class TaggingProject extends React.PureComponent<{}> {
   render() {
     return (
       <Placeholder description="Tagging Project">
-        <TagYourProject>
-          <Placeholder description="Tagging Instructions" />
-          <Placeholder description="Tag Options">
-            <Placeholder description="Table" />
-            <Placeholder description="Chairs" />
-          </Placeholder>
-        </TagYourProject>
+        <Placeholder description="Tagging Header" />
+        <Placeholder description="Tagging Instructions" />
+        <TagOptions />
       </Placeholder>
     );
   }

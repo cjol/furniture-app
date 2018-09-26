ab322451c1592b54c292c9ac1ca83167343fa48a
user004 <user004@tyrohq.com>
2018-09-26 15:59:58 -0700

    2.4-IconStyling-Stars

--- src/components/ProfilePage/ProfileDetails/ProfileRatings/ProfileRatings.tsx
+++ src/components/ProfilePage/ProfileDetails/ProfileRatings/ProfileRatings.tsx
@@ -3,25 +3,30 @@ import { Placeholder } from "Components/Placeholder";
 import { Star } from "@material-ui/icons";
 import { StarBorder } from "@material-ui/icons";
 import styled from "styled-components";
+import { greenMain, grey } from "@style";
 
-const RatingsStyle=styled.div`
-display: flex;
-flex-direction: row;
-justify-content: center;
+const RatingsStyle = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
 `;
-
-
+const StarStyle = styled(Star)`
+  color: ${greenMain};
+`;
+const StarBorderStyle = styled.div`
+  border-color: ${grey};
+`;
 export class ProfileRatings extends React.PureComponent<{}> {
   static defaultProps: ProfileRatings["props"] = {};
 
   render() {
     return (
       <RatingsStyle>
-        <Star />
-        <Star />
-        <Star />
-        <Star />
-        <StarBorder />
+        <StarStyle />
+        <StarStyle />
+        <StarStyle />
+        <StarStyle />
+        <StarBorderStyle />
       </RatingsStyle>
     );
   }

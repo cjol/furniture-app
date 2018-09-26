a6e877dcf7702cd0c766c3111019ea9e0f93f293
Christopher Little <christopher@tyrohq.com>
2018-09-26 12:31:56 +0300

    Merge branch '2.1+2.2-BidResolutionPage-UserContactInfo'

--- src/components/BidResolutionPage/UserContactInfo/EmailRow/EmailRow.tsx
+++ src/components/BidResolutionPage/UserContactInfo/EmailRow/EmailRow.tsx
@@ -4,16 +4,19 @@ import { Call, Email } from "@material-ui/icons";
 import styled from "styled-components";
 const EmailStyle = styled.div`
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
+  justify-content: stretch;
 `;
-const EmailaddressStyle = styled.div``;
+const EmailaddressStyle = styled.div`
+  flex-grow: 1;
+`;
 export class EmailRow extends React.PureComponent<{}> {
   static defaultProps: EmailRow["props"] = {};
 
   render() {
     return (
       <EmailStyle>
-        <EmailaddressStyle />
+        <EmailaddressStyle>Email@email.com</EmailaddressStyle>
         <Email />
       </EmailStyle>
     );

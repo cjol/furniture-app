33e97fd144d4aba01ded47be3ed8e1c1611c3474
Christopher Little <christopher@tyrohq.com>
2018-09-26 12:15:36 +0300

    Give image size

--- src/components/BidResolutionPage/UserContactInfo/InfoRow/InfoRowPicture/InfoRowPicture.tsx
+++ src/components/BidResolutionPage/UserContactInfo/InfoRow/InfoRowPicture/InfoRowPicture.tsx
@@ -3,6 +3,8 @@ import { Placeholder } from "Components/Placeholder";
 import styled from "styled-components";
 const InfoRowPictureStyled = styled.div`
   display: flex;
+  height: 50px;
+  width: 50px;
 `;
 export class InfoRowPicture extends React.PureComponent<{}> {
   static defaultProps: InfoRowPicture["props"] = {};

e987d5cc0b3d831b99d722efc0685d7ffb6c8d52
Christopher Little <christopher@tyrohq.com>
2018-09-26 12:37:19 +0300

    Merge branch '2.1+2.2ProfilePage-EditSave'

--- src/components/ProfilePage/ActiveProjectBids/ActiveProjectBids.tsx
+++ src/components/ProfilePage/ActiveProjectBids/ActiveProjectBids.tsx
@@ -7,7 +7,7 @@ import styled from "styled-components";
 const ActiveProjectBidsStyle = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center;
+  align-items: strech;
 `;
 
 export class ActiveProjectBids extends React.PureComponent<{}> {

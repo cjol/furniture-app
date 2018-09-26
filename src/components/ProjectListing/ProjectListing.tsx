cb539650a06d5c692c13fa3ee6c47edd21c1204b
User 001 <user001@tyrohq.com>
2018-09-26 10:47:26 +0300

    Completed-2.1-2.2 ProjectListing

--- src/components/ProjectListing/ProjectListing.tsx
+++ src/components/ProjectListing/ProjectListing.tsx
@@ -1,17 +1,25 @@
 import * as React from "react";
 import { Placeholder } from "Components/Placeholder";
 import { ProjectInfo } from "./ProjectInfo/ProjectInfo";
+import styled from "styled-components";
+
+const ProjectListingStyle = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
 export class ProjectListing extends React.PureComponent<{}> {
   static defaultProps: ProjectListing["props"] = {};
 
   render() {
     return (
-      <Placeholder description="Projectdetails">
-        <Placeholder description="ProjectImage" />
-        <ProjectInfo />
-        <Placeholder description="Amount" />
-      </Placeholder>
+      <ProjectListingStyle>
+        <Placeholder description="Projectdetails">
+          <Placeholder description="ProjectImage" />
+          <ProjectInfo />
+          <Placeholder description="Amount" />
+        </Placeholder>
+      </ProjectListingStyle>
     );
   }
 }

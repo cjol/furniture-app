91cfabf93a9215e0a6134d8873591bfc930918da
user004 <user004@tyrohq.com>
2018-09-26 15:24:59 -0700

    2.4-Subheader

--- src/components/Subheading/Subheading.tsx
+++ src/components/Subheading/Subheading.tsx
@@ -1,8 +1,13 @@
 import * as React from "react";
 import { Placeholder } from "Components/Placeholder";
 import styled from "styled-components";
+import { mainFont, greenMain } from "@style";
 const SubHeadingStyled = styled.div`
   display: flex;
+  justify-content: flex-start;
+  color: greenMain;
+  font-family: ${mainFont};
+  font-size: 18px;
 `;
 export class Subheading extends React.PureComponent<{}> {
   static defaultProps: Subheading["props"] = {};

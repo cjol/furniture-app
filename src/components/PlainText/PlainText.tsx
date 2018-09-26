591c62a4d31dabfd97fecb0e5ca22541de355e80
user004 <user004@tyrohq.com>
2018-09-26 14:24:36 -0700

    2.1-PlainText

--- src/components/PlainText/PlainText.tsx
+++ src/components/PlainText/PlainText.tsx
@@ -1,10 +1,12 @@
 import * as React from "react";
 import { Placeholder } from "Components/Placeholder";
+import styled from "styled-components";
+const SomeTextStyle = styled.div``;
 
 export class PlainText extends React.PureComponent<{}> {
   static defaultProps: PlainText["props"] = {};
 
   render() {
-    return <Placeholder description="Some Text" />;
+    return <SomeTextStyle>Text Area</SomeTextStyle>;
   }
 }

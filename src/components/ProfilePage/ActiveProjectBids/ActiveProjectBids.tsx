196c44201ff1f41babf65f26e727d5bde752ea19
User 001 <user001@tyrohq.com>
2018-09-26 12:19:48 +0300

    Completed 2.1+2.2 ProfiePage-ActiveProjectBids

--- src/components/ProfilePage/ActiveProjectBids/ActiveProjectBids.tsx
+++ src/components/ProfilePage/ActiveProjectBids/ActiveProjectBids.tsx
@@ -2,17 +2,24 @@ import * as React from "react";
 import { Placeholder } from "Components/Placeholder";
 import { ProjectListing } from "Components/ProjectListing/ProjectListing";
 import { Subheading } from "Components/Subheading/Subheading";
+import styled from "styled-components";
+
+const ActiveProjectBidsStyle = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
 export class ActiveProjectBids extends React.PureComponent<{}> {
   static defaultProps: ActiveProjectBids["props"] = {};
 
   render() {
     return (
-      <Placeholder description="Activeprojects/Bids" orientation="column">
+      <ActiveProjectBidsStyle>
         <Subheading />
         <ProjectListing />
         <ProjectListing />
-      </Placeholder>
+      </ActiveProjectBidsStyle>
     );
   }
 }

2ce87216288a2bfb0466ff0da90baa6e15d6540f
user004 <user004@tyrohq.com>
2018-09-26 10:16:21 -0700

    2.2-ProfileImage

--- src/components/ProfilePage/ProfileDetails/ProfileImage/ProfileImage.tsx
+++ src/components/ProfilePage/ProfileDetails/ProfileImage/ProfileImage.tsx
@@ -2,11 +2,13 @@ import * as React from "react";
 import styled from "styled-components";
 import { Placeholder } from "Components/Placeholder";
 
-const ImageStyle = styled.div``
+const ImageStyle = styled.div`
+  display: flex;
+`;
 export class ProfileImage extends React.PureComponent<{}> {
   static defaultProps: ProfileImage["props"] = {};
 
   render() {
-    return <ImageStyle/>;
+    return <ImageStyle />;
   }
 }

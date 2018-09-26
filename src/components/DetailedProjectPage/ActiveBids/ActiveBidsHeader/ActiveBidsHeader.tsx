1dee729b83f11ffc82e6cec3b84b05276cf8ed67
user005 <user005@tyrohq.com>
2018-09-26 11:16:49 -0700

    2.1-2.2DetailedProjectPageActiveBids-ActiveBidsHeader

--- src/components/DetailedProjectPage/ActiveBids/ActiveBidsHeader/ActiveBidsHeader.tsx
+++ src/components/DetailedProjectPage/ActiveBids/ActiveBidsHeader/ActiveBidsHeader.tsx
@@ -1,21 +1,26 @@
-import * as React from "react";
-import { Placeholder } from "Components/Placeholder";
-import { ArrowDropDown } from "@material-ui/icons";
-import { ArrowDropUp } from "@material-ui/icons";
+import { ArrowDropDown, ArrowDropUp } from "@material-ui/icons";
 import { Subheading } from "Components/Subheading/Subheading";
+import * as React from "react";
+import styled from "styled-components";
+
+const ActiveBidsHeaderStyle = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+const ArrowsStyle = styled.div``;
 
 export class ActiveBidsHeader extends React.PureComponent<{}> {
   static defaultProps: ActiveBidsHeader["props"] = {};
 
   render() {
     return (
-      <Placeholder description="ActiveBidsHeader">
+      <ActiveBidsHeaderStyle>
         <Subheading />
-        <Placeholder description="arrows" orientation="row">
+        <ArrowsStyle>
           <ArrowDropDown />
           <ArrowDropUp />
-        </Placeholder>
-      </Placeholder>
+        </ArrowsStyle>
+      </ActiveBidsHeaderStyle>
     );
   }
 }

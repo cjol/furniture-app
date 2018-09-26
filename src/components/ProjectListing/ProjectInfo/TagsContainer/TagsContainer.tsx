947e27a4426db3ad7f18c3436979d6922c99523c
User 002 <user002@tyrohq.com>
2018-09-26 14:55:07 +0300

    2.1+2.2-ProjectListings-ProjectInfo-TagsContainer

--- src/components/ProjectListing/ProjectInfo/TagsContainer/TagsContainer.tsx
+++ src/components/ProjectListing/ProjectInfo/TagsContainer/TagsContainer.tsx
@@ -1,16 +1,27 @@
 import * as React from "react";
 import { Placeholder } from "Components/Placeholder";
+import styled from "styled-components";
+
+const TagsContainerStyle = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  flex-grow: 1;
+`;
+const TagsStyle = styled.div``;
+const Tag1Style = styled.div``;
+const Tag2Style = styled.div``;
 
 export class TagsContainer extends React.PureComponent<{}> {
   static defaultProps: TagsContainer["props"] = {};
 
   render() {
     return (
-      <Placeholder description="Tags Container" orientation="row">
-        <Placeholder description="Tags: " />
-        <Placeholder description="Tag 1" />
-        <Placeholder description="Tag 2" />
-      </Placeholder>
+      <TagsContainerStyle>
+        <TagsStyle>Tags:</TagsStyle>
+        <Tag1Style>Bed</Tag1Style>
+        <Tag2Style>Bedside Table</Tag2Style>
+      </TagsContainerStyle>
     );
   }
 }

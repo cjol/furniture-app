5522c6d45287f8e2847ecffe7b2a2fd3e2ab0f46
user003 <user003@tyrohq.com>
2018-09-26 10:43:15 -0700

    2.1+2.2-AppHeader

--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -1,16 +1,35 @@
 import * as React from "react";
 import { Placeholder } from "Components/Placeholder";
 import { Menu } from "@material-ui/icons";
+import styled from "styled-components";
+
+const MenuBarStyle = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const TitleStyle = styled.div`
+  display: flex;
+  flex-grow: 1;
+  font-family: "Arial";
+  font-size: 15px;
+  font-weight: bold;
+`;
+
+const MenuStyle = styled.div`
+  display: flex;
+  justify-content: flex-start;
+`;
 
 export class AppHeader extends React.PureComponent<{}> {
   static defaultProps: AppHeader["props"] = {};
 
   render() {
     return (
-      <Placeholder description="MenuBar" orientation="row">
-        <Menu/>
-        <Placeholder description="Title" />
-      </Placeholder>
+      <MenuBarStyle>
+        <MenuStyle />
+        <TitleStyle />
+      </MenuBarStyle>
     );
   }
 }

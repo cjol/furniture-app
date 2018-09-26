0301f93cae58e35ac4e435bfd1c24cc96fad5503
user004 <user004@tyrohq.com>
2018-09-26 12:39:00 -0700

    2.1+2.2-RectangularButton

--- src/components/RectangularButton/RectangularButton.tsx
+++ src/components/RectangularButton/RectangularButton.tsx
@@ -1,10 +1,21 @@
 import * as React from "react";
 import { Placeholder } from "Components/Placeholder";
+import styled from "styled-components";
+const RectangularButtonStyle = styled.div`
+  display: flex;
+`;
+const ButtonLabelStyle = styled.div`
+  align-items: center;
+`;
 
 export class RectangularButton extends React.PureComponent<{}> {
   static defaultProps: RectangularButton["props"] = {};
 
   render() {
-    return <Placeholder description="Button Label" />;
+    return (
+      <RectangularButtonStyle>
+        <ButtonLabelStyle>Text</ButtonLabelStyle>
+      </RectangularButtonStyle>
+    );
   }
 }

7e5b3167a48ed479d4204bc904a3c136c996c2ec
user003 <user003@tyrohq.com>
2018-09-26 15:18:06 -0700

    2.4-RectangularButton-ButtonLabel

--- src/components/RectangularButton/RectangularButton.tsx
+++ src/components/RectangularButton/RectangularButton.tsx
@@ -1,16 +1,17 @@
 import * as React from "react";
 import { Placeholder } from "Components/Placeholder";
 import styled from "styled-components";
-import  {mainColor} "@style";
+import  {darkerGreen} "@style";
+import {mainFont} "@style";
 const RectangularButtonStyle = styled.div`
   display: flex;
 `;
 const ButtonLabelStyle = styled.div`
   align-items: center;
-  font-family: "Arial" serif;
+  font-family: ${mainFont};
   font-size: 14px;
   color: white;
-  background-color: ${mainColor}
+  background-color: ${darkerGreen},
   text-align: center;
 `;
 

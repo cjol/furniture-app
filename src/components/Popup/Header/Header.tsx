8cabb2994b2687f449baed782cf57ad46964c692
user004 <user004@tyrohq.com>
2018-09-26 15:14:06 -0700

    2.4-PopupHeader

--- src/components/Popup/Header/Header.tsx
+++ src/components/Popup/Header/Header.tsx
@@ -2,10 +2,16 @@ import * as React from "react";
 import { Placeholder } from "Components/Placeholder";
 import { Close } from "@material-ui/icons";
 import styled from "styled-components";
+import { mainFont } from "@style";
 
 const HeaderStyle = styled.div`
   display: flex;
   flex-direction: row;
+  justify-content: stretch;
+  font-family: ${mainFont};
+  font-weight: bold;
+  color: black;
+  font-size: 20px;
 `;
 
 const PopupTitleStyle = styled.div`

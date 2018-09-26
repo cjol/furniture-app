440180a782c73eb3c011d847434b42495bd5b7a2
User 001 <user001@tyrohq.com>
2018-09-26 10:12:39 +0300

    Completed 2.1-2.2-CreateProjectPageStyle

--- src/components/CreateProjectPage/CreateProjectPage.tsx
+++ src/components/CreateProjectPage/CreateProjectPage.tsx
@@ -4,28 +4,30 @@ import { Placeholder } from "Components/Placeholder";
 import { ProjectHeader } from "Components/ProjectHeader/ProjectHeader";
 import { RectangularButton } from "Components/RectangularButton/RectangularButton";
 import * as React from "react";
-
 import { PhotoContainer } from "./PhotoContainer/PhotoContainer";
 import { TaggingProject } from "./TaggingProject/TaggingProject";
 import { ProjectDescription } from "./ProjectDescription/ProjectDescription";
+import styled from "styled-components";
+
+const CreateProjectPageStyle = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
 export class CreateProjectPage extends React.PureComponent<{}> {
   static defaultProps: CreateProjectPage["props"] = {};
 
   render() {
     return (
-      <Placeholder description="Page" orientation="column">
+      <CreateProjectPageStyle>
         <AppHeader />
         <ProjectHeader />
         <PhotoContainer />
         <ImageBar />
-
         <ProjectDescription />
-
         <TaggingProject />
-
         <RectangularButton>POST PROJECT</RectangularButton>
-      </Placeholder>
+      </CreateProjectPageStyle>
     );
   }
 }

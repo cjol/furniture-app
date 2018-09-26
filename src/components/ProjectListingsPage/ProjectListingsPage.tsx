f3cd69ecd6ff480fb7661a6040c351f2b0880a14
user005 <user005@tyrohq.com>
2018-09-26 12:30:52 -0700

    2.1-2.2ProjectListingsPage

--- src/components/ProjectListingsPage/ProjectListingsPage.tsx
+++ src/components/ProjectListingsPage/ProjectListingsPage.tsx
@@ -5,7 +5,10 @@ import { AppHeader } from "Components/AppHeader/AppHeader";
 import { SearchContainer } from "./SearchContainer/SearchContainer";
 import { ListingsContainer } from "./ListingsContainer/ListingsContainer";
 import styled from "styled-components";
-const PageStyle = styled.div``;
+const PageStyle = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
 export class ProjectListingsPage extends React.PureComponent<{}> {
   static defaultProps: ProjectListingsPage["props"] = {};

ef60a57e3080c01e3e0db069bee3944bed469b32
user005 <user005@tyrohq.com>
2018-09-26 12:14:03 -0700

    2.1-2.2BidresolutionPage-UsercontactInfo-EmailRow

--- src/components/BidResolutionPage/UserContactInfo/EmailRow/EmailRow.tsx
+++ src/components/BidResolutionPage/UserContactInfo/EmailRow/EmailRow.tsx
@@ -1,16 +1,21 @@
 import * as React from "react";
 import { Placeholder } from "Components/Placeholder";
 import { Call, Email } from "@material-ui/icons";
-
+import styled from "styled-components";
+const EmailStyle = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const EmailaddressStyle = styled.div``;
 export class EmailRow extends React.PureComponent<{}> {
   static defaultProps: EmailRow["props"] = {};
 
   render() {
     return (
-      <Placeholder description="email">
-        <Placeholder description="email address" orientation="row" />
+      <EmailStyle>
+        <EmailaddressStyle />
         <Email />
-      </Placeholder>
+      </EmailStyle>
     );
   }
 }

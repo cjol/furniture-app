4eeb3b39c7ee0eb665e6d9e34b5ff037b5af8e9a
User 001 <user001@tyrohq.com>
2018-09-26 12:02:35 +0300

    Completed 2.1+2.2 BidResolutionPage-UserContactInfo-Address

--- src/components/BidResolutionPage/UserContactInfo/Address/Address.tsx
+++ src/components/BidResolutionPage/UserContactInfo/Address/Address.tsx
@@ -1,14 +1,26 @@
 import * as React from "react";
 import { Placeholder } from "Components/Placeholder";
+import styled from "styled-components";
+
+const AddressHeadingStyle = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+`;
+const AddressStyle = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+`;
 
 export class Address extends React.PureComponent<{}> {
   static defaultProps: Address["props"] = {};
 
   render() {
     return (
-      <Placeholder description="Address Heading">
-        <Placeholder description="Address" />
-      </Placeholder>
+      <AddressHeadingStyle>
+        <AddressStyle>ADDRESS</AddressStyle>
+      </AddressHeadingStyle>
     );
   }
 }

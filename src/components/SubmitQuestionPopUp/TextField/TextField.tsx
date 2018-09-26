56b544b65a2eb32abbd2cf3d279eddf6ae21b340
User 002 <user002@tyrohq.com>
2018-09-26 12:37:46 +0300

    2.1-SubmitQuestion-TextField

--- src/components/SubmitQuestionPopUp/TextField/TextField.tsx
+++ src/components/SubmitQuestionPopUp/TextField/TextField.tsx
@@ -1,10 +1,13 @@
 import * as React from "react";
 import { Placeholder } from "Components/Placeholder";
+import styled from "styled-components";
+
+const TextFieldStyle = styled.div``;
 
 export class TextField extends React.PureComponent<{}> {
   static defaultProps: TextField["props"] = {};
 
   render() {
-    return <Placeholder description="TextField" />;
+    return <TextFieldStyle />;
   }
 }

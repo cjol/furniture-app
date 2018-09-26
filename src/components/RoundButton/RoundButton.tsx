3490eab387e2457b271f6170ed6c8f720cdd4e6c
user004 <user004@tyrohq.com>
2018-09-26 15:46:44 -0700

    2.4-RoundButton

--- src/components/RoundButton/RoundButton.tsx
+++ src/components/RoundButton/RoundButton.tsx
@@ -2,9 +2,12 @@ import * as React from "react";
 import { Placeholder } from "Components/Placeholder";
 import { Add } from "@material-ui/icons";
 import styled from "styled-components";
+import { greenMain, white } from "@style";
 
 const RoundButtonStyled = styled.div`
   display: flex;
+  background-color: ${greenMain};
+  color: ${white};
 `;
 export class RoundButton extends React.PureComponent<{}> {
   static defaultProps: RoundButton["props"] = {

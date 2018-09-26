da2261ab40e988a8f56be823cc367f58b5e2e44a
user003 <user003@tyrohq.com>
2018-09-26 12:34:17 -0700

    2.1+2.2-BidResolutionPage-CostDue

--- src/components/BidResolutionPage/CostDue/CostDue.tsx
+++ src/components/BidResolutionPage/CostDue/CostDue.tsx
@@ -1,15 +1,31 @@
 import * as React from "react";
 import { Placeholder } from "Components/Placeholder";
+import styled from "styled-components";
+
+const CostDueStyle = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const ProjectCostStyle = styled.div`
+  display: flex;
+  justify-content: flex-start;
+`;
+
+const ProjectDueStyle = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`;
 
 export class CostDue extends React.PureComponent<{}> {
   static defaultProps: CostDue["props"] = {};
 
   render() {
     return (
-      <Placeholder description="Cost-Due" orientation="row">
-        <Placeholder description="Project Cost" />
-        <Placeholder description="Project Due" />
-      </Placeholder>
+      <CostDueStyle>
+        <ProjectCostStyle>text</ProjectCostStyle>
+        <ProjectDueStyle>text</ProjectDueStyle>
+      </CostDueStyle>
     );
   }
 }

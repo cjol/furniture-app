7a3dd732571e0551adb539d722deea35f5a7d8cf
Hama <Hama@Owamparo.local>
2018-09-27 09:40:08 +0300

    2.4SearchContainerIconStylingComplete

--- src/components/ProjectListingsPage/SearchContainer/SearchContainer.tsx
+++ src/components/ProjectListingsPage/SearchContainer/SearchContainer.tsx
@@ -3,11 +3,14 @@ import { Placeholder } from "Components/Placeholder";
 import { Search } from "@material-ui/icons";
 import { Subheading } from "Components/Subheading/Subheading";
 import styled from "styled-components";
+import { Checkbox } from "@material-ui/core";
 const SearchHeaderStyle = styled.div`
   display: flex;
   flex-direction: row;
 `;
-
+const SearchCheckBoxStyle =styled(Checkbox)`
+color:grey;
+`;
 export class SearchContainer extends React.PureComponent<{}> {
   static defaultProps: SearchContainer["props"] = {};
 

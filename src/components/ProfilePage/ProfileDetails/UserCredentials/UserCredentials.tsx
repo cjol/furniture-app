8e7b9ec78795204cb410056933affc43c22528d3
Christopher Little <christopher@tyrohq.com>
2018-09-27 23:39:54 +0300

    Add state for profile page form

--- src/components/ProfilePage/ProfileDetails/UserCredentials/UserCredentials.tsx
+++ src/components/ProfilePage/ProfileDetails/UserCredentials/UserCredentials.tsx
@@ -25,6 +25,8 @@ const UserCredentialsTitleStyle = styled.input`
 
 export class UserCredentials extends React.PureComponent<{
   isEditMode: boolean;
+  setPictureURL: (picture: string) => void;
+  pictureURL: string;
   setFullName: (fullName: string) => void;
   fullName: string;
   setEmail: (email: string) => void;
@@ -33,31 +35,34 @@ export class UserCredentials extends React.PureComponent<{
   location: string;
   setPhoneNumber: (phoneNumber: string) => void;
   phoneNumber: string;
-  setPassword: (password: string) => void;
-  password: string;
+  // setPassword: (password: string) => void;
+  // password: string;
 }> {
   static defaultProps: UserCredentials["props"] = {
     isEditMode: true,
-    fullName: " ",
+    setPictureURL: picture => alert("Inserting picture"),
+    pictureURL: " ",
     setFullName: fullName => alert("Inserting FullName"),
+    fullName: " ",
     setEmail: email => alert("Inserting Email"),
     email: " ",
     setLocation: location => alert("Inserting location"),
     location: " ",
     setPhoneNumber: phoneNumber => alert("Inserting PhoneNumber"),
-    phoneNumber: " ",
-    setPassword: password => alert("Inserting Password"),
-    password: " "
+    phoneNumber: " "
+    // setPassword: password => alert("Inserting Password"),
+    // password: " "
   };
 
   render() {
     return (
       <UserCredentialsStyle>
         <UserCredentialsTitleStyle value="FullName" />
+        <UserCredentialsTitleStyle value="Picture URL" />
         <UserCredentialsTitleStyle type="email" value="Email Address" />
         <UserCredentialsTitleStyle value="Location" />
         <UserCredentialsTitleStyle value="PhoneNumber" />
-        <UserCredentialsTitleStyle type="password" value="Password" />
+        {/* <UserCredentialsTitleStyle type="password" value="Password" /> */}
       </UserCredentialsStyle>
     );
   }
--- src/components/ProfilePage/ProfilePage.tsx
+++ src/components/ProfilePage/ProfilePage.tsx
@@ -6,13 +6,14 @@ import { ActiveProjectBids } from "./ActiveProjectBids/ActiveProjectBids";
 import { SuppliersSelected } from "./SuppliersSelected/SuppliersSelected";
 import { ProfileDetails } from "./ProfileDetails/ProfileDetails";
 import styled from "styled-components";
-import {white}from "@style";
+import { white } from "@style";
+import { ProfilePageState } from "./ProfilePageState";
 
 const ProfilePageHeaderStyle = styled.div`
   display: flex;
   flex-direction: column;
-  padding:24px;
-  background-color:${white};
+  padding: 24px;
+  background-color: ${white};
 `;
 
 export class ProfilePage extends React.PureComponent<{}> {
@@ -20,13 +21,49 @@ export class ProfilePage extends React.PureComponent<{}> {
 
   render() {
     return (
-      <ProfilePageHeaderStyle>
-        <AppHeader />
-        <ProfileDetails />
-        <ProfileEditSave />
-        <ActiveProjectBids />
-        <SuppliersSelected />
-      </ProfilePageHeaderStyle>
+      <ProfilePageState>
+        {({
+          fullName,
+          setFullName,
+
+          pictureURL,
+          setPictureURL,
+
+          email,
+          setEmail,
+
+          location,
+          setLocation,
+
+          phoneNumber,
+          setPhoneNumber
+        }) => {
+          return (
+            <ProfilePageHeaderStyle>
+              <AppHeader />
+              <ProfileDetails
+                profileImageprops={{ image: pictureURL }}
+                userCredentialsprops={{
+                  isEditMode: true,
+                  fullName,
+                  setFullName,
+                  pictureURL,
+                  setPictureURL,
+                  email,
+                  setEmail,
+                  phoneNumber,
+                  setPhoneNumber,
+                  location,
+                  setLocation
+                }}
+              />
+              <ProfileEditSave />
+              <ActiveProjectBids />
+              <SuppliersSelected />
+            </ProfilePageHeaderStyle>
+          );
+        }}
+      </ProfilePageState>
     );
   }
 }

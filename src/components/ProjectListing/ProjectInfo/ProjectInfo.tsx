3f433d3ebbe45c2d8be817fff5e05d8b0b336070
user003 <user003@tyrohq.com>
2018-09-27 10:13:30 -0700

    2.4-ProjectListing-ProjectInfo-Title

--- src/components/ProjectListing/ProjectInfo/ProjectInfo.tsx
+++ src/components/ProjectListing/ProjectInfo/ProjectInfo.tsx
@@ -2,11 +2,22 @@ import * as React from "react";
 import { Placeholder } from "Components/Placeholder";
 import { TagsContainer } from "./TagsContainer/TagsContainer";
 import styled from "styled-components";
+import {mainFont} from "@style";
+import {black} from "@style";
+
+
 const ProjectInfoStyle = styled.div`
   display: flex;
   flex-direction: column;
 `;
-const ProjectTitleStyle = styled.div``;
+const ProjectTitleStyle = styled.div`
+display:flex;
+font-family:${mainFont}
+font-size:18px;
+color: ${black};
+font-weight: bold;
+
+`;
 
 export class ProjectInfo extends React.PureComponent<{}> {
   static defaultProps: ProjectInfo["props"] = {};

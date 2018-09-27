ee467353d55efe1d891771f959e6f4b41cbd8ef6
Christopher Little <christopher@tyrohq.com>
2018-09-27 10:27:58 +0300

    Semicolon

--- src/components/ProjectListing/ProjectInfo/ProjectInfo.tsx
+++ src/components/ProjectListing/ProjectInfo/ProjectInfo.tsx
@@ -2,21 +2,19 @@ import * as React from "react";
 import { Placeholder } from "Components/Placeholder";
 import { TagsContainer } from "./TagsContainer/TagsContainer";
 import styled from "styled-components";
-import {mainFont} from "@style";
-import {black} from "@style";
-
+import { mainFont } from "@style";
+import { black } from "@style";
 
 const ProjectInfoStyle = styled.div`
   display: flex;
   flex-direction: column;
 `;
 const ProjectTitleStyle = styled.div`
-display:flex;
-font-family:${mainFont}
-font-size:18px;
-color: ${black};
-font-weight: bold;
-
+  display: flex;
+  font-family: ${mainFont};
+  font-size: 18px;
+  color: ${black};
+  font-weight: bold;
 `;
 
 export class ProjectInfo extends React.PureComponent<{}> {

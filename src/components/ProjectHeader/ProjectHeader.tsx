b92b431709d32f5273b73bb26f0df7401125f355
user004 <user004@tyrohq.com>
2018-09-27 12:13:06 -0700

    2.3-ProjectHeaderStyle

--- src/components/ProjectHeader/ProjectHeader.tsx
+++ src/components/ProjectHeader/ProjectHeader.tsx
@@ -1,13 +1,16 @@
 import { Placeholder } from "Components/Placeholder";
 import { UserDetails } from "Components/UserDetails/UserDetails";
 import * as React from "react";
-
+import { greenMain } from "@style";
 import { ProjectTitle } from "./ProjectTitle/ProjectTitle";
 import styled from "styled-components";
+
 const ProjectHeaderStyle = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
+  padding: 16px;
+  background-color: ${greenMain};
 `;
 
 export class ProjectHeader extends React.PureComponent<{}> {

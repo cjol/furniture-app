6cf1786c5884750e132571a4210b5ec2d89e1237
Hama <Hama@Owamparo.local>
2018-09-27 12:02:40 +0300

    2.3ProfilePgProfileDetailsSupplieSelectedCompleted

--- src/components/ProfilePage/SuppliersSelected/SuppliersSelected.tsx
+++ src/components/ProfilePage/SuppliersSelected/SuppliersSelected.tsx
@@ -7,6 +7,8 @@ import styled from "styled-components";
 const SuppliersSelectedStyled = styled.div`
   display: flex;
   flex-direction: column;
+  margin-top:;24px
+  margin-bottom:24px;
 `;
 export class SuppliersSelected extends React.PureComponent<{}> {
   static defaultProps: SuppliersSelected["props"] = {};

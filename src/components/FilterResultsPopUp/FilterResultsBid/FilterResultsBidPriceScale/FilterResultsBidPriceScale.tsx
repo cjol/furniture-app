1ad1ddc6dc8d48a20980247845e43f3916298510
Christopher Little <christopher@tyrohq.com>
2018-09-27 11:52:46 +0300

    Add scale bar style

--- src/components/FilterResultsPopUp/FilterResultsBid/FilterResultsBidPriceScale/FilterResultsBidPriceScale.tsx
+++ src/components/FilterResultsPopUp/FilterResultsBid/FilterResultsBidPriceScale/FilterResultsBidPriceScale.tsx
@@ -1,33 +1,65 @@
 import * as React from "react";
 import { Placeholder } from "Components/Placeholder";
 import styled from "styled-components";
-import { grey } from "@style";
+import { grey, greenMain } from "@style";
 import { globalAgent } from "https";
 
-const BidPriceScaleTextStyle = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
-const BidPricesRangestyle=styled.div`
-color: ${grey};
-  font-size: 11px;`;
-const FilterResultsBidPriceScaleStyle = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: stretch;
-  position: relative;
-  flex-grow: 1;
-`;
 const CircleStyle = styled.div`
   border-radius: 10px;
   height: 10px;
   width: 10px;
-  position: absolute;
+  background-color: ${greenMain};
+  position: relative;
+  z-index: 1;
 `;
-const FilledBarStyle = styled.div`
+const BarStyle = styled.div`
+  position: absolute;
+  width: 100%;
+  top: 16px;
+  height: 3px;
+  border-radius: 4px;
+`;
+const FilledBarStyle = styled(BarStyle)`
+  z-index: -1;
+  background-color: ${greenMain};
+  height: 4px;
+`;
+const UnfilledBarStyle = styled(BarStyle)`
+  z-index: -2;
+  background-color: ${grey};
+`;
+
+const FilledScaleContainerStyle = styled.div`
+  display: flex;
+  position: relative;
+  flex-grow: 1;
+  flex-direction: column;
+  max-width: 50%;
+  left: 25%;
+`;
+const ScaleContainerStyle = styled.div`
+  display: flex;
+  position: relative;
+  flex-grow: 1;
+  flex-direction: column;
+`;
+
+const ScaleCircleContainerStyle = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
   flex-grow: 1;
 `;
-globalAgent
+const BidPricesRangestyle = styled.div`
+  color: ${grey};
+  font-size: 11px;
+`;
+const ScaleLabelContainerStyle = styled.div`
+  display: flex;
+  flex-grow: 1;
+  justify-content: space-between;
+`;
+
 export class FilterResultsBidPriceScale extends React.PureComponent<{
   top: number;
   bottom: number;
@@ -43,14 +75,20 @@ export class FilterResultsBidPriceScale extends React.PureComponent<{
 
   render() {
     return (
-      <BidPriceScaleTextStyle>
-          <BidPricesRangestyle/>
-          <FilterResultsBidPriceScaleStyle>
+      <ScaleContainerStyle>
+        <FilledScaleContainerStyle>
+          <ScaleLabelContainerStyle>
+            <BidPricesRangestyle>250k</BidPricesRangestyle>
+            <BidPricesRangestyle>1M</BidPricesRangestyle>
+          </ScaleLabelContainerStyle>
+          <ScaleCircleContainerStyle>
             <CircleStyle />
-            <FilledBarStyle />
             <CircleStyle />
-          </FilterResultsBidPriceScaleStyle>
-      </BidPriceScaleTextStyle>
+          </ScaleCircleContainerStyle>
+          <FilledBarStyle />
+        </FilledScaleContainerStyle>
+        <UnfilledBarStyle />
+      </ScaleContainerStyle>
     );
   }
 }

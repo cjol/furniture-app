eef5eba84ebe6c6b01534b35551808f30c856270
Hama <Hama@Owamparo.local>
2018-09-27 09:30:16 +0300

    2.4BidResolutionPageUserInfoComplete

--- src/components/BidResolutionPage/UserContactInfo/EmailRow/EmailRow.tsx
+++ src/components/BidResolutionPage/UserContactInfo/EmailRow/EmailRow.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { Call, Email } from "@material-ui/icons";
 import { Placeholder } from "Components/Placeholder";
 import styled from "styled-components";
+import { Checkbox } from "@material-ui/core";
 const EmailStyle = styled.div`
   display: flex;
   flex-grow: 1;
@@ -9,8 +10,15 @@ const EmailStyle = styled.div`
   justify-content: stretch;
 `;
 const EmailaddressStyle = styled.div`
+display:flex;
   flex-grow: 1;
+  color:grey;
 `;
+const EmailCheckBoxStyle = styled(Checkbox)`
+color:grey;
+`;
+
+
 export class EmailRow extends React.PureComponent<{ email: string }> {
   static defaultProps: EmailRow["props"] = { email: "address@email.com" };
 

c18e282a6af58a4c9a8b7d6f4a43da4e8f603b7d
user004 <user004@tyrohq.com>
2018-09-27 09:25:34 -0700

    2.4-Address

--- src/components/BidResolutionPage/UserContactInfo/Address/Address.tsx
+++ src/components/BidResolutionPage/UserContactInfo/Address/Address.tsx
@@ -1,17 +1,25 @@
 import * as React from "react";
 import { Placeholder } from "Components/Placeholder";
 import styled from "styled-components";
+import { white, grey } from "@style";
 
 const AddressContentStyle = styled.div`
   display: flex;
   flex-direction: column;
   align-items: flex-start;
+  font-size: 13px;
+  background-color: ${white};
 `;
-const AddressHeadingStyle = styled.div``;
+const AddressHeadingStyle = styled.div`
+  color: ${grey};
+  font-weight: bold;
+`;
 const AddressStyle = styled.div`
   display: flex;
   flex-direction: row;
   align-items: flex-start;
+  color: ${grey};
+  text-decoration: underline;
 `;
 
 export class Address extends React.PureComponent<{

97de860f2d43bb74c17647987de871d0dc9d0641
User 002 <user002@tyrohq.com>
2018-09-27 09:54:56 +0300

    3.1-CreateprojectPage/Taggingproject

--- src/components/PlainText/PlainText.tsx
+++ src/components/PlainText/PlainText.tsx
@@ -5,10 +5,10 @@ import { mainFont } from "@style";
 import { grey } from "@style";
 
 export const PlainText = styled.div`
-display:flex;
-font-family:${mainFont}
-font-size: 13px;
-color:${grey};
-text-align: justify;
-line-spacing: 2em;
+  display: flex;
+  font-family: ${mainFont};
+  font-size: 13px;
+  color: ${grey};
+  text-align: justify;
+  line-spacing: 2em;
 `;

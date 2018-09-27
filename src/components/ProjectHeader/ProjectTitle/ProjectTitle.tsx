d82bb9e006e24abc17b996a9dd5d3cb9628d080d
user003 <user003@tyrohq.com>
2018-09-27 16:03:43 -0700

    3.1-ProjectHeader-ProjectTitle

--- src/components/ProjectHeader/ProjectTitle/ProjectTitle.tsx
+++ src/components/ProjectHeader/ProjectTitle/ProjectTitle.tsx
@@ -1,19 +1,26 @@
 import * as React from "react";
 import styled from "styled-components";
 import { mainFont } from "@style";
-import {darkerGreen} from "@style"
+import { darkerGreen } from "@style";
+
 const ProjectTitleStyle = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: center;
   font: ${mainFont};
   font-size: 24px;
-  padding:16px;
-  background-color:${darkerGreen};
+  padding: 16px;
+  background-color: ${darkerGreen};
 `;
 
-export class ProjectTitle extends React.PureComponent<{}> {
-  static defaultProps: ProjectTitle["props"] = {};
+export class ProjectTitle extends React.PureComponent<{
+  title: string;
+  isEditable: boolean;
+}> {
+  static defaultProps: ProjectTitle["props"] = {
+    title: "title goes here",
+    isEditable: true
+  };
 
   render() {
     return <ProjectTitleStyle>Title Goes Here</ProjectTitleStyle>;

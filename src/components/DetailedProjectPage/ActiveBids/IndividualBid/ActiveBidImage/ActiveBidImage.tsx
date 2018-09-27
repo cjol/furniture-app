ed5647f361ea825f7295e80c3bcbc056f0cd445a
sharifahblessing <snaluswata18@gmail.com>
2018-09-27 10:24:44 +0300

    Fixed 3.2 IndividualBid-ActiveBidImage

--- src/components/DetailedProjectPage/ActiveBids/IndividualBid/ActiveBidImage/ActiveBidImage.tsx
+++ src/components/DetailedProjectPage/ActiveBids/IndividualBid/ActiveBidImage/ActiveBidImage.tsx
@@ -2,18 +2,19 @@ import * as React from "react";
 import { Placeholder } from "Components/Placeholder";
 import styled from "styled-components";
 
-const ActiveBidImageStyle = styled.div`
+const ActiveBidImageStyle = styled.div<{image:string}>`
   display: flex;
   flex-direction: row;
   align-items: flex-start;
   flex-grow: 0;
   border: solid;
+  background-image: url(${props =>{return props.image}});
 `;
 
 export class ActiveBidImage extends React.PureComponent<{image:string;}> {
   static defaultProps: ActiveBidImage["props"] = {image:"www.tyrohq.com/logo.png"};
 
   render() {
-    return <ActiveBidImageStyle>{this.props.image}</ActiveBidImageStyle>;
+    return <ActiveBidImageStyle image={this.props.image}></ActiveBidImageStyle>;
   }
 }

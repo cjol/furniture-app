16836187b3151ee9772ae860e55b7eb61e029f63
sharifahblessing <snaluswata18@gmail.com>
2018-09-27 10:42:31 +0300

    Completed 3.1 Create ProjectData

--- src/components/CreateProjectPage/CreateProjectPage.tsx
+++ src/components/CreateProjectPage/CreateProjectPage.tsx
@@ -14,8 +14,24 @@ const CreateProjectPageStyle = styled.div`
   flex-direction: column;
 `;
 
-export class CreateProjectPage extends React.PureComponent<{}> {
-  static defaultProps: CreateProjectPage["props"] = {};
+export class CreateProjectPage extends React.PureComponent<{
+  appHeaderProps: AppHeader["props"];
+  projectHeaderProps: ProjectHeader["props"];
+  photoContainerProps: PhotoContainer["props"];
+  imageBarProps: ImageBar["props"];
+  projectDescriptionProps: ProjectDescription["props"];
+  taggingProject: TaggingProject["props"];
+  onClick: () => void;
+}> {
+  static defaultProps: CreateProjectPage["props"] = {
+    appHeaderProps: AppHeader.defaultProps,
+    projectHeaderProps: ProjectHeader.defaultProps,
+    photoContainerProps: PhotoContainer.defaultProps,
+    imageBarProps: ImageBar.defaultProps,
+    projectDescriptionProps: ProjectDescription.defaultProps,
+    taggingProject: TaggingProject.defaultProps,
+    onClick: () => alert("Posting Project")
+  };
 
   render() {
     return (

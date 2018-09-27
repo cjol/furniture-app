1c8748fe5dbaa86728218acf1bb5504f9350a9fe
user004 <user004@tyrohq.com>
2018-09-27 11:00:29 -0700

    2.4-ProjectTagsSubHeading

--- src/components/FilterResultsPopUp/FilterResultsProjectTags/FilterResultsProjectTags.tsx
+++ src/components/FilterResultsPopUp/FilterResultsProjectTags/FilterResultsProjectTags.tsx
@@ -2,11 +2,17 @@ import { Placeholder } from "Components/Placeholder";
 import { ProjectTag } from "./ProjectTag/ProjectTag";
 import * as React from "react";
 import styled from "styled-components";
+import { black } from "@style";
 
 const FilterResultsProjectTagsStyled = styled.div`
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
+  justify-content: flex-start;
+`;
+const ProjectTagSubHeadingStyle = styled.div`
+  font-size: 18px;
+  color: ${black};
 `;
 export class FilterResultsProjectTags extends React.PureComponent<{}> {
   static defaultProps: FilterResultsProjectTags["props"] = {};
@@ -14,6 +20,7 @@ export class FilterResultsProjectTags extends React.PureComponent<{}> {
   render() {
     return (
       <FilterResultsProjectTagsStyled>
+        <ProjectTagSubHeadingStyle>ProjectTags</ProjectTagSubHeadingStyle>
         <ProjectTag />
         <ProjectTag />
         <ProjectTag />

30d53515ceced8c4f59689db410cf971e66e4aff
user004 <user004@tyrohq.com>
2018-09-27 15:16:07 -0700

    2.3-InfoRow

--- src/components/BidResolutionPage/UserContactInfo/InfoRow/InfoRow.tsx
+++ src/components/BidResolutionPage/UserContactInfo/InfoRow/InfoRow.tsx
@@ -5,10 +5,13 @@ import { Call, Email } from "@material-ui/icons";
 import { InfoRowPicture } from "./InfoRowPicture/InfoRowPicture";
 import { InfoNameAndNumber } from "./InfoNameAndNumber/InfoNameAndNumber";
 import styled from "styled-components";
+import { greenMain } from "@style";
 
 const InfoRowStyled = styled.div`
   display: flex;
   flex-direction: row;
+  background-color: ${greenMain};
+  padding: 24px;
 `;
 export class InfoRow extends React.PureComponent<{
   infoRowPictureProps: InfoRowPicture["props"];

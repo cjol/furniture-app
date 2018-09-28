380c5b4a7f795534505c349b8e9c25e8675c2af6
Hama <Hama@Owamparo.local>
2018-09-28 16:14:42 +0300

    4.2projectListingsComplete

--- src/components/ProjectListingsPage/ProjectListingsPage.tsx
+++ src/components/ProjectListingsPage/ProjectListingsPage.tsx
@@ -6,6 +6,10 @@ import { SearchContainer } from "./SearchContainer/SearchContainer";
 import { ListingsContainer } from "./ListingsContainer/ListingsContainer";
 import styled from "styled-components";
 import { white } from "@style";
+import {FetchProjectListings} from "./FetchProjectListings.gql";
+import {FetchProjectListings as FetchProjectListingsType} from "@data";
+import {Query} from "react-apollo";
+import {Loading, DataError,} from "Components/Placeholder";
 
 const PageStyle = styled.div`
   display: flex;
@@ -29,13 +33,39 @@ export class ProjectListingsPage extends React.PureComponent<{
 
   render() {
     return (
-      <PageStyle>
+      <Query<FetchProjectListingsType> query={FetchProjectListings}>
+      
+      {({loading, error, data}) => {
+        if (loading) return <Loading/>;
+        if (error) return <DataError/>;
+
+        const listings = data.listProjects.items.map(item => {
+          return {
+            id: item.id,
+            projectImage: item.photos[0],
+            amountTitle:item.averageBidAmount,
+            projectInfoProps:{
+              title: item.title,
+              tagsContainerProps: {
+                tags: item.tags
+              }
+            },
+            chosenBid: item.averageBidAmount
+
+          }
+        })
+
+      return <PageStyle>
         <AppHeader {...this.props.appHeaderProps} />
         <PageContents>
           <SearchContainer />
-          <ListingsContainer {...this.props.listingsContainerProps} />
+          <ListingsContainer 
+          projectListingProps={listings} />
         </PageContents>
-      </PageStyle>
+     
+     </PageStyle>
+      }}
+     </Query>
     );
   }
 }

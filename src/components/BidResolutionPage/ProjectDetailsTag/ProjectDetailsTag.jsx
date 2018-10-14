32eca0f89b54dacc0f264b165a6867a578083298
Christopher Little <christopher@tyrohq.com>
2018-10-14 13:47:33 +0300

    add ProjectDetailsTag

---
+++ src/components/BidResolutionPage/ProjectDetailsTag/ProjectDetailsTag.jsx
@@ -0,0 +1,25 @@
+
+import * as React from "react";
+import styled from "styled-components";
+
+const ProjectDetailsTagStyle = styled.div`
+    display: flex;
+    flex-direction: row;
+`
+
+const LabelStyle = styled.div`
+`
+
+const ValueStyle = styled.div`
+`
+
+export class ProjectDetailsTag extends React.PureComponent<{}> {
+  static defaultProps: ProjectDetailsTag["props"] = {};
+
+  render() {
+    return <ProjectDetailsTagStyle>
+        <LabelStyle>Project Cost:</LabelStyle>
+        <ValueStyle>500,000 UGX</ValueStyle>
+    </ProjectDetailsTagStyle>
+  }
+}
